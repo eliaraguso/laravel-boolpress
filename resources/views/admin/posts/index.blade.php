523f69af29b4935a4647a0dae69b31b6991690b2
eliaraguso <elia.raguso@gmail.com>
2021-11-16 16:28:01 +0100

    aggiunta la pagina admin/posts per visualizzare tutti i post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,11 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('Dashboard') }}</div> 
+                    
 
                     <div class="card-body">
                         <table class="table">
-                            <thead>
+                            <thead></thead>
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Title</th>
